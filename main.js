359777a8d442a316fb09959fd776c855550b8e2d
Muhammadjon <ibodullamuhammad@gmail.com>
2022-03-11 16:45:26 +0500

    dars

---
+++ main.js
@@ -0,0 +1,26 @@
+// const btns=document.querySelectorAll("button"),
+// wrapper=document.querySelector(".wrapper");
+
+// wrapper.addEventListener("click",(event)=>{
+//     if(event.target && event.target.tagName=="BUTTON"){
+//         console.log("bosildi");
+//     }
+// })
+// wrapper.addEventListener('click',(event)=>{
+
+//     if(event.target&& event.target.matches('button.blue')){
+//         console.log('bosildi');
+//     }
+// })
+
+// // foreach yordamida qilish
+// btns.forEach(btn=>{
+//     btn.addEventListener('click',()=>{
+//         console.log('click');
+//     })
+// })
+
+
+// const btn=document.createElement('button')
+// btn.classList.add('crimson')
+// wrapper.append(btn)
